ab7b15da372d348bb1f11494c08667de1ec4616f
jamesonarnett <jamesonarnett@yahoo.com>
2022-11-13 19:42:39 -0600

    Breaks bun, playing with AI responses

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,41 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { useState, useCallback } from "react";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
+  const [value, setValue] = useState<string>("");
+  const [prompt, setPrompt] = useState<string>("");
+  const [completion, setCompletion] = useState<string>("");
+
+  const handleInput = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }, []);
+
+  const handleKeyDown = useCallback(
+    async (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        setPrompt(value);
+        setCompletion("Loading...");
+        const response = await fetch("/api/hello", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ text: value }),
+        });
+        const data = await response.json();
+        setValue("");
+        setCompletion(data.result.choices[0].text);
+      }
+    },
+    [value]
+  );
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>nextBunBase</title>
+        <title>nextBunBase-bb</title>
         <meta
           name="description"
           content="Playing with Next13, pocketBase, and Bun"
@@ -15,15 +44,28 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
+        <div className={styles.titleDiv}>
+          <h1 className={styles.title}>Hello</h1>
+        </div>
+        <div className={styles.inputDiv}>
+          <p className={styles.description}>Type in a prompt</p>
+          <input
+            value={value}
+            onChange={handleInput}
+            onKeyDown={handleKeyDown}
+          />
+          <div>
+            Prompt: <span>{prompt}</span>
+          </div>
+          <div>
+            Completion:{" "}
+            <span>
+              {completion.split("\n").map((item) => (
+                <>{item}</>
+              ))}
+            </span>
+          </div>
+        </div>
         <div className={styles.grid}></div>
       </main>
 
